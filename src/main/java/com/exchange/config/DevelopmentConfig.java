ce8c056cba63a779c5bb7231c0650131130f176d
Nguyễn Mai Lam <nguyenlamit86@gmail.com>
2017-01-23 12:01:59 +0700

    Updated config exchange develop branch

--- src/main/java/com/exchange/config/DevelopmentConfig.java
+++ src/main/java/com/exchange/config/DevelopmentConfig.java
@@ -1,8 +1,5 @@
 package com.exchange.config;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
@@ -15,10 +12,5 @@ import org.springframework.context.annotation.PropertySource;
 @Profile("dev")
 @PropertySource("config/application-dev.properties")
 public class DevelopmentConfig {
-    @Bean
-    public ServletRegistrationBean h2ConsoleServletRegistration(){
-        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
-        bean.addUrlMappings("/console/*");
-        return bean;
-    }
+
 }
