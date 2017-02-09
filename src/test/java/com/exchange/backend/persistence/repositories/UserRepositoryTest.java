e6230abe5986585851a18feceaafc9c585293b25
Mr Lam <Mr Lam>
2017-02-09 18:56:13 +0700

    Added user responsitory and service

--- src/test/java/com/exchange/backend/persistence/repositories/UserRepositoryTest.java
+++ src/test/java/com/exchange/backend/persistence/repositories/UserRepositoryTest.java
@@ -6,19 +6,19 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Mrs Hoang on 08/02/2017.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringApplicationConfiguration(ExchangeApplication.class)
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
-
 
     @Test
     public void test() throws Exception{
