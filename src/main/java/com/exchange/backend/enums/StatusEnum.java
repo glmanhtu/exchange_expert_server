6735c927bbca6c7353d9da77eacb768ab55b4039
Mr Lam <Mr Lam>
2017-02-12 21:52:53 +0700

    Implemented Rest api create goods

---
+++ src/main/java/com/exchange/backend/enums/StatusEnum.java
@@ -0,0 +1,27 @@
+package com.exchange.backend.enums;
+
+/**
+ * Created by Mrs Hoang on 12/02/2017.
+ */
+public enum StatusEnum {
+
+    PENDING("Pending",""),
+    TRADING("Trading","");
+
+    private String name;
+
+    private String description;
+
+    StatusEnum(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- src/main/java/com/exchange/backend/service/GoodService.java
+++ src/main/java/com/exchange/backend/service/GoodService.java
@@ -11,6 +11,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +40,9 @@ public class GoodService implements SearchEverything<Good> {
      * @see Good
      */
     public Good create(Good good) {
+
+        good.setPostDate(LocalDateTime.now(Clock.systemDefaultZone()));
+
         return goodRepository.save(good);
     }
 
