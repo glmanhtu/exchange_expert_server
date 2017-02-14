e6e88e1cb2ef67af782bcd76c61e2478a49dc511
Mr Lam <Mr Lam>
2017-02-14 10:00:16 +0700

    Added message and messageEnum to control errors response

--- src/main/java/com/exchange/restapi/GoodHandler.java
+++ src/main/java/com/exchange/restapi/GoodHandler.java
@@ -1,12 +1,11 @@
 package com.exchange.restapi;
 
-import com.exchange.backend.enums.MessageType;
+import com.exchange.backend.enums.MessageEnum;
 import com.exchange.backend.enums.StatusEnum;
 import com.exchange.backend.persistence.domain.Good;
-import com.exchange.backend.persistence.domain.MessageDTO;
+import com.exchange.backend.persistence.domain.Message;
 import com.exchange.backend.persistence.domain.Status;
 import com.exchange.backend.service.GoodService;
-import com.exchange.backend.service.I18NService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,46 +16,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 /**
  * Created by Mrs Hoang on 12/02/2017.
  */
 @RestController
-@RequestMapping(GoodHandler.REST_API_GOODS)
 public class GoodHandler {
 
     /** The application logger */
     private static final Logger LOGGER = LoggerFactory.getLogger(GoodHandler.class);
 
     public static final String REST_API_GOODS = "/goods";
+    public static final String REST_API_GOOD_CREATE = REST_API_GOODS + "/create";
 
     @Autowired
     private GoodService goodService;
-
-    @Autowired
-    private I18NService i18NService;
-
-    private List<MessageDTO> messageDTOS;
 
     /**
      * Creates a new good given by good
      * @param good
      * @param locale
-     * @return MessageDTO after create.
-     * @see MessageDTO
+     * @return Message after create.
      * @see Locale
      * @see Good
      */
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<Object> createGoods(@RequestBody Good good, Locale locale) {
 
-        //construct messageDTOs
-        messageDTOS = new ArrayList<>();
-
-        LOGGER.info("Creating gooods {}", good);
+        LOGGER.info("Creating goods {}", good);
 
         //create good id given by user id and good slug
         String goodId = good.getPostBy().getId() + good.getSlug();
@@ -65,11 +53,8 @@ public class GoodHandler {
         if (goodService.inValidSlug(good.getSlug())) {
 
             LOGGER.error("The slug {} is invalid", good.getSlug());
-
-            messageDTOS.add(new MessageDTO(MessageType.ERROR,
-                    i18NService.getMessage("goods.error.slug.invalid.text", good.getSlug(), locale)));
-
-            return new ResponseEntity<Object>(messageDTOS, HttpStatus.BAD_REQUEST);
+            Message message = new Message(MessageEnum.GOODS_SLUG_INVALID);
+            return new ResponseEntity<Object>(message, HttpStatus.BAD_REQUEST);
         }
 
         good.setStatus(new Status(StatusEnum.PENDING));
@@ -79,9 +64,6 @@ public class GoodHandler {
 
         LOGGER.info("Created goods {}", good);
 
-        messageDTOS.add(new MessageDTO(MessageType.SUCCESS,
-                i18NService.getMessage("goods.create.success.text", good.getTitle(), locale)));
-
-        return new ResponseEntity<Object>(messageDTOS, HttpStatus.OK);
+        return new ResponseEntity<Object>(good, HttpStatus.OK);
     }
 }
