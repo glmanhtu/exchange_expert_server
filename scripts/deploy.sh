0dd412907870e09202d2a7df0c923e46d3d854fc
Optimize <tuvm@optimize.vn>
2017-02-22 22:53:12 +0700

    Update profile active

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo `chmod +x service.sh`
-echo `./service.sh stop -e Production -jar /exchange_expert/release/exchange-0.0.1-SNAPSHOT.jar`
+echo `./service.sh stop -e dev -jar /exchange_expert/release/exchange-0.0.1-SNAPSHOT.jar`
 echo `rm -rf /exchange_expert/release/exchange-0.0.1-SNAPSHOT.jar`
 echo `cp /home/ubuntu/exchange-0.0.1-SNAPSHOT.jar /exchange_expert/release/`
-echo `./service.sh start -e Production -jar /exchange_expert/release/exchange-0.0.1-SNAPSHOT.jar`
+echo `./service.sh start -e dev -jar /exchange_expert/release/exchange-0.0.1-SNAPSHOT.jar`
