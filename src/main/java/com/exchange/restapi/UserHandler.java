5a721ef2b6b1c6a4a768fd7558765fb425b759a5
Nguyễn Mai Lam <nguyenlamit86@gmail.com>
2017-02-24 16:48:33 +0700

    Implement api create user

--- src/main/java/com/exchange/restapi/UserHandler.java
+++ src/main/java/com/exchange/restapi/UserHandler.java
@@ -10,15 +10,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Mrs Hoang on 12/02/2017.
  */
 @RestController
+@RequestMapping(UserHandler.REST_API_USER)
 public class UserHandler {
 
     /** The application logger */
@@ -32,7 +34,30 @@ public class UserHandler {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = REST_API_USER, method = RequestMethod.GET)
+    /**
+     * Creates new user given by user.
+     *
+     * @param user
+     * @return A user DTO or exception if email has been existed
+     * @see MessageEnum
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+
+        if (userService.getOne(user.getId()) != null) {
+            Message message = new Message(MessageEnum.USER_EMAIL_INVALID);
+            return new ResponseEntity<>(message, HttpStatus.BAD_GATEWAY);
+        }
+
+        user = userService.create(user);
+
+        //convert to user dto
+        UserDto userDto = new UserDto(user);
+
+        return new ResponseEntity<Object>(userDto, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<?> getUser(@RequestParam("email") String email) {
 
         User user = userService.getOne(email);
