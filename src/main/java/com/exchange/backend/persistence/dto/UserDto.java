0fc75572632c0b87e80c4cb34c02ccb8a8a18a63
Nguyễn Mai Lam <nguyenlamit86@gmail.com>
2017-02-26 10:30:26 +0700

    Fixed error null exception field

--- src/main/java/com/exchange/backend/persistence/dto/UserDto.java
+++ src/main/java/com/exchange/backend/persistence/dto/UserDto.java
@@ -31,14 +31,20 @@ public class UserDto {
 
     public UserDto(User user) {
 
-        this.id = user.getId();
-        this.firstName = user.getFirstName();
-        this.lastName = user.getLastName();
-        this.birthday = user.getBirthday();
-        this.avatar = user.getAvatar();
-        this.createDate = user.getCreateDate();
-        this.gender = user.getGender();
-        this.avgRating = user.getRating().getAvg();
+        if(user.getId() != null)
+            this.id = user.getId();
+        if(user.getFirstName() != null)
+            this.firstName = user.getFirstName();
+        if(user.getLastName() != null)
+            this.lastName = user.getLastName();
+        if(user.getBirthday() != null)
+            this.birthday = user.getBirthday();
+        if(user.getAvatar() != null)
+            this.avatar = user.getAvatar();
+        if(user.getCreateDate() != null)
+            this.createDate = user.getCreateDate();
+        if(user.getRating() != null)
+            this.avgRating = user.getRating().getAvg();
     }
 
     public String getId() {
