ccb18ba8cc20483aa1eeed43efb34c5b3c7b0374
Lam Nguyễn <lamnguyenmai@hvct.edu.vn>
2017-04-10 15:43:42 +0700

    Change response structure admin search good

--- src/main/java/com/exchange/restapi/AdminHandler.java
+++ src/main/java/com/exchange/restapi/AdminHandler.java
@@ -7,13 +7,15 @@ import com.exchange.backend.enums.StatusEnum;
 import com.exchange.backend.persistence.domain.Good;
 import com.exchange.backend.persistence.domain.Message;
 import com.exchange.backend.persistence.domain.Status;
-import com.exchange.backend.persistence.dto.SimpleGoodDto;
+import com.exchange.backend.persistence.dto.GoodDto;
 import com.exchange.backend.service.GoodService;
 import com.exchange.restapi.request.GoodStatus;
 import org.elasticsearch.common.unit.DistanceUnit;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -91,8 +93,14 @@ public class AdminHandler {
         if (searchGood.getSeller() != null) {
             queryBuilder.must(QueryBuilders.termQuery("postBy.id", searchGood.getSeller()));
         }
-        List<SimpleGoodDto> simpleGoodDtos = new ArrayList<>();
-        goodService.findAll(queryBuilder, pageRequest).forEach(good -> simpleGoodDtos.add(new SimpleGoodDto(good)));
-        return new ResponseEntity<>(simpleGoodDtos, HttpStatus.OK);
+
+        List<GoodDto> goodDtos = new ArrayList<>();
+
+        Page<Good> goods = goodService.findAll(queryBuilder, pageRequest);
+        goods.getContent().forEach(good -> goodDtos.add(new GoodDto(good)));
+
+        Page<GoodDto> response = new PageImpl<GoodDto>(goodDtos, pageRequest, goods.getTotalElements());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
