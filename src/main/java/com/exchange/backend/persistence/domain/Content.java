cb53929c67b1dfae0b0ffbe96924251c5e5e9b8f
Lam Nguyễn <lamnguyenmai@hvct.edu.vn>
2017-04-20 12:34:47 +0700

    Fixed multi rating user and style checking

--- src/main/java/com/exchange/backend/persistence/domain/Content.java
+++ src/main/java/com/exchange/backend/persistence/domain/Content.java
@@ -60,8 +60,12 @@ public class Content {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Content content = (Content) o;
 
